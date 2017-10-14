cf05043e6377346f990abd0ba6131757de355f65
amiller120 <adam5bucks475@gmail.com>
2017-10-14 09:04:55 -0400

    Added commit statement

--- create_db.py
+++ create_db.py
@@ -4,4 +4,5 @@ with sqlite3.connect('security1.db') as conn:
     c.execute("""CREATE TABLE log 
                 (date1 text PRIMARY KEY , policy text, port int, source_address text, source_zone text, destination_address,
                  destination_zone)""")
+    conn.commit()
 
